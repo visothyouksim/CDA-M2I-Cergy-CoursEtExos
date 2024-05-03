3d614e9c3db0c07bba1f158b3e51b4ffe4c35145
MaDesOcr <madesocr@gmail.com>
2024-05-03 09:39:36 +0200

    Commit SmallProject2TakeANoteInit
    Enoncé et structure du code

---
+++ SmallProjet2TakeANote/src/main/SmallProjet2TakeNote.java
@@ -0,0 +1,26 @@
+package main;
+
+public class SmallProjet2TakeNote {
+
+	//TODO
+		//création d'un outil de prise de note
+		//une petite interface texte doit vous demander si vous voulez enregistrer une note
+		
+		//choix du menu:
+		//prendre une note
+		//En bonus :lire la liste des notes
+		
+		//fonctions : 
+		//.enregistrement d'une note (un nouveau fichier txt par note, se baser sur la date)
+		//		
+		
+		//En bonus : .enregistrement dans une liste de notes
+		//			chaque nouvelle note vera son nom ajouté à la liste
+		//
+		
+	
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ SmallProjet2TakeANote/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SmallProjet2TakeANote {
+}
